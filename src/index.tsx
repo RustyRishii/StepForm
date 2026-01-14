46dcc65e9f9117a668c1bbaa7dd2bd5be83e924a
RustyRishii <rishikeshbidkar04@gmail.com>
2026-01-14 05:41:57 +0530

    Finally got the component working

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-// // src/index.ts
-// export { default as RenderStepForm } from "./RenderStepForm";
-// // export type { RenderStepFormProps, StepConfig } from "./RenderStepForm";
-
-// src/index.ts
-export { default as RenderStepForm } from "./RenderStepForm";
